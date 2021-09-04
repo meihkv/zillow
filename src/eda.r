0e9bcdbe4371d2ad9580ededeaf7ccc60941b59d
meihkv <mjvuong@gmail.com>
2021-09-04 18:40:11 -0400

    group SalesCount by year

--- src/eda.r
+++ src/eda.r
@@ -1,5 +1,6 @@
 library(tidyverse)
-
+library(lubridate)
+library(plotly)
 sales_count = read.csv('./data/Metro_sales_count_now_uc_sfrcondo_month.csv',colClasses = 'character')
 
 str(sales_count)
@@ -8,4 +9,11 @@ sales_count_pivot = sales_count %>%
     pivot_longer(col = starts_with('X'),
                  names_to = 'Date',
                  values_to = 'SalesCount') %>% 
-  mutate(Date = as.Date(Date, tryFormats = c("X%Y.%m.%d")))
+  mutate(Date = as.Date(Date, tryFormats = c("X%Y.%m.%d"))) %>% 
+  mutate(SalesCount = as.numeric(SalesCount)) %>% 
+  mutate(Year = floor_date(Date, unit = 'year')) %>% 
+  group_by(StateName, Year) %>% 
+  summarise(SalesCount = sum(SalesCount)) %>% 
+  ungroup() %>% 
+  mutate(StateName = if_else(StateName == '', 'US', StateName)) %>% 
+  pivot_wider(names_from = StateName, values_from = SalesCount)
